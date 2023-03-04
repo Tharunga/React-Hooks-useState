0e9e59e082785563256402b641adc6e26d50b147
Tharunga Kuruppu <112683072+Tharunga@users.noreply.github.com>
2023-03-04 13:05:41 +0530

    upload node modules before run code

---
+++ src/app.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import './style.css';
+
+function App(){
+
+    const now = new Date().toLocaleTimeString();
+    
+    const[time, setTime] = useState(now)
+
+
+    setInterval(updateTime, 1000);
+
+    function updateTime() {
+        const newTime = new Date().toLocaleTimeString();
+        setTime(newTime) ;
+    }
+
+    return(
+        <div className="container">
+            <h1>{time}</h1>
+            <button onClick={updateTime}>Get Time</button>
+        </div>
+
+    );
+}
+
+export default App;
